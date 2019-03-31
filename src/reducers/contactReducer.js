0e24a52df5b02f9d6f320a3254ca1dd085510686
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-31 20:03:25 -0300

    Intanciando contatos no initial state e definindo o retorno do get_contacts

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,8 +1,35 @@
-const initialState = {}
+import { GET_CONTACTS, ADD_CONTACT } from '../actions/types';
+
+const initialState = {
+  contacts: [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      phone: '555-555-5555'
+    },
+    {
+      id: 2,
+      name: 'Karen Williams',
+      email: 'karen@gmail.com',
+      phone: '444-444-4444'
+    },
+    {
+      id: 3,
+      name: 'Henry Johnson',
+      email: 'henry@gmail.com',
+      phone: '333-333-333'
+    }
+  ]
+}
 
 export default function(state = initialState, action) {
   switch(action.type) {
-    case 'ADD_CONTACT':
+    case GET_CONTACTS:
+      return {
+        ...state
+      }
+    case ADD_CONTACT:
       return {
         ...state,
         contacts: 'Exemplo'
