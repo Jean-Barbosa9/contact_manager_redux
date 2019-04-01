05581698385f9303dc25116c1a7af2cf550e8aca
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-31 23:14:22 -0300

    Deixando a versão dinâmica, conforme versão do projeto é aumentada

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { version } from '../../../package.json';
 
 export default () => {
   return (
     <div>
       <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple app to manage contacts</p>
-      <p className="text-secondary">Version 1.0.0</p>
+      <p className="text-secondary">Version {version}</p>
     </div>
   );
 };
