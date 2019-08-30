5604605f02cdeb6175801f3ace156f5d36c589de
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-08-29 23:01:50 -0300

    Corrigindo declaração do Redux Devtools
    Da forma como estava quebrava a aplicação no Firefox e agora também só
    está disponível em ambiente de desenvolvimento.

--- src/store.js
+++ src/store.js
@@ -1,16 +1,27 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const REDUX__DEVTOOLS__EXTENSION =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const initialState = {},
   middleware = [thunk],
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__&&window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
-  );
+  store =
+    window.location.port >= 3000 &&
+    window.navigator.userAgent.indexOf("Chrome") >= 0
+      ? createStore(
+          rootReducer,
+          initialState,
+          compose(
+            applyMiddleware(...middleware),
+            REDUX__DEVTOOLS__EXTENSION
+          )
+        )
+      : createStore(
+          rootReducer,
+          initialState,
+          compose(applyMiddleware(...middleware))
+        );
 
 export default store;
