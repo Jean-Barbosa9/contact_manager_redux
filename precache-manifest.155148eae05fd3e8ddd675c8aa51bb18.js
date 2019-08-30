83f167134631dd985ea07bf3cf5faabe2f7251cb
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-08-29 23:06:06 -0300

    Updates

---
+++ precache-manifest.155148eae05fd3e8ddd675c8aa51bb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "562e0da6ee8c657e5f2b",
+    "url": "/contact_manager_redux/static/js/runtime~main.337fdefb.js"
+  },
+  {
+    "revision": "abe82f2c463cb24a5e3f",
+    "url": "/contact_manager_redux/static/js/main.6b506dbb.chunk.js"
+  },
+  {
+    "revision": "2f19d109989e62732ba1",
+    "url": "/contact_manager_redux/static/js/2.561ae4ea.chunk.js"
+  },
+  {
+    "revision": "abe82f2c463cb24a5e3f",
+    "url": "/contact_manager_redux/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "2f19d109989e62732ba1",
+    "url": "/contact_manager_redux/static/css/2.8076407e.chunk.css"
+  },
+  {
+    "revision": "0bbe049918a4e4475c648a2bf140d23e",
+    "url": "/contact_manager_redux/index.html"
+  }
+];
